7606ff75a936c22d499f0c4eec96bcf3207cacd6
coiluck <wvy.wingle@gmail.com>
2026-04-26 23:43:27 +0900

    Add: android settings

---
+++ src-tauri/gen/android/app/src/main/java/moe/coiluck/kokone_music/MainActivity.kt
@@ -0,0 +1,19 @@
+package moe.coiluck.kokone_music
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : TauriActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { view, insets ->
+            val statusBarInsets = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+            view.setPadding(0, statusBarInsets.top, 0, 0)
+            insets
+        }
+    }
+}
